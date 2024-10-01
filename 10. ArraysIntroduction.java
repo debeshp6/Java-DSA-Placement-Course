e7756da7a8951eacd2ffa26f15e82e993efa9d35
Debesh Paul <139678494+debeshp6@users.noreply.github.com>
2024-10-02 02:09:21 +0530

    Create 10. ArraysIntroduction.java

--- 10. ArraysIntroduction.java
+++ 10. ArraysIntroduction.java
@@ -111,3 +111,27 @@ public class Arrays {
 
 
 // arrayName.length => size of that array
+
+
+/* Qs. Take an array as input from user. Search for a given number x and print the index at which it occurs. */
+import java.util.*;
+public class SearchArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+        int size = sc.nextInt();
+        int numbers[] = new int[size];
+        // input
+        for(int i=0; i<size; i++) {
+            numbers[i] = sc.nextInt();
+        }
+        System.out.print("Element to be searched: ");
+        int x = sc.nextInt();
+        // output
+        for(int i=0; i<numbers.length; i++) {
+            if(numbers[i] == x) {
+                System.out.println("x found at index: " + i);
+            }
+        }
+    }
+}
