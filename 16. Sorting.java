7809bcd993ea182eb60db327ded34bb7c690cc48
Debesh Paul <139678494+debeshp6@users.noreply.github.com>
2024-10-08 02:02:41 +0530

    Create 16. Sorting.java

--- 16. Sorting.java
+++ 16. Sorting.java
@@ -27,4 +27,58 @@ public class BubbleSort {
 
 
 
+/* <--------------- Selection Sort---------------> */
+import java.util.*;
+public class SelectionSort {
+    // time complexity = O(n^2)
+    public static void printArray(int arr[]) {
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {7,8,3,1,2};
+        for(int i=0; i<arr.length-1; i++) {
+            int smallest = i;
+            for(int j=i+1; j<arr.length; j++) {
+                if(arr[j] < arr[smallest]) {
+                    smallest = j;
+                }
+            }
+            // swap
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+        printArray(arr);
+    }
+}
 
+
+
+/* <--------------- Insertion Sort---------------> */
+import java.util.*;
+public class SelectionSort {
+    // time complexity = O(n^2)
+    public static void printArray(int arr[]) {
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {7,8,3,1,2};
+        for(int i=1; i<arr.length; i++) {
+            int current = arr[i];
+            int j = i-1;
+            while(j >= 0 && arr[j] > current) {
+                // keep swapping
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = current;
+        }
+        printArray(arr);
+    }
+}
