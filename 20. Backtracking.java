99c313f6cf7350cba5621099eb35d6f1b1a119e0
Debesh Paul <139678494+debeshp6@users.noreply.github.com>
2024-11-07 04:55:12 +0530

    Create 20. Backtracking.java

---
+++ 20. Backtracking.java
@@ -0,0 +1,27 @@
+/*
+
+remember that detergent brand named 'Ghari'?
+if you do, then surely you remember their famous tagline - "Pehle istemaal karein, fir vishwas karein"
+if we simplify the term 'Backtracking', the meaning is same.
+
+cool, right? B)
+
+simple definition --> find all possible solutions & use the one you want.
+
+example: arrange 3 kids in a single line. [permutation concept]
+  1. kid A
+  2. kid B
+  3. kid C
+
+total permutations = n! = 3! = 3 x 2 x 1 = 6
+
+=> {ABC, ACB, BAC, BCA, CAB, CBA}
+
+same these types of quesions when we try to solve with Recursion, we use the concept of Backtracking.
+
+a recursion tree is a visual representation of a recursive algorithm, illustrating how the function calls itself repeatedly until it reaches a base case.
+in java, recursion is a powerful technique for solving problems by breaking them down into smaller, more manageable sub-problems.
+
+
+
+*/
