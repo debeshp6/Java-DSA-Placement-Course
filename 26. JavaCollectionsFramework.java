1fa78960bce49794c260aa2a8129d5a8933eab65
Debesh Paul <139678494+debeshp6@users.noreply.github.com>
2024-11-11 05:18:36 +0530

    Create 26. JavaCollectionsFramework.java

---
+++ 26. JavaCollectionsFramework.java
@@ -0,0 +1,21 @@
+/*
+
+the java platform includes a collections framework. a collection is an object that represents a group of objects.
+a collections framework is a unified architecture for representing and manipulating collections, enabling collections to be manipulated
+independently of implementation details. the primary advantages of a collections framework are that it:
+
+  >> reduces programming effort by providing data structures and algorithms so you don't have to write them yourself.
+  >> increases performance by providing high-performance implementations of data structures and algorithms.
+  because the various implementations of each interface are interchangeable, programs can be tuned by switching implementations.
+  >> provides interoperability between unrelated APIs by establishing a common language to pass collections back and forth.
+
+  ETC!
+  
+actually it's 5 a.m. and i am still going strong - no breaks, just the grind :)
+
+BUT,
+if you want, you can go through this whole documentation --> https://docs.oracle.com/javase/8/docs/technotes/guides/collections/overview.html
+
+
+  
+*/
