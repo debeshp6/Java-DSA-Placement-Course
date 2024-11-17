70a20c272e4199a7dd59a064af330403847e2b57
Debesh Paul <139678494+debeshp6@users.noreply.github.com>
2024-11-17 17:06:46 +0530

    Update 29. MostImportantLinkedListQuestions.java

--- 29. MostImportantLinkedListQuestions.java
+++ 29. MostImportantLinkedListQuestions.java
@@ -6,8 +6,6 @@
 
 
 
-
-
 /* Q1. Find the nth node from the end & remove it. */
 
 /* 
@@ -22,6 +20,8 @@ distance from start (prev node) => size - n => 5 - 2 = 3
 */
 
 class Solution {
+    //time complexity -> O(n)
+    //space complexity -> O(1)
     public ListNode removeNthFromEnd(ListNode head, int n) {
         if(head.next == null) {
             return null;
@@ -34,9 +34,25 @@ class Solution {
             size++;
         }
         
-        int indexToSearch = size - n;
+        if(n == size) {
+            return head.next;
+        }
         
+        int indexToSearch = size - n;
+        ListNode prev = head;
+        int i = 1;
+        while(i < indexToSearch) {
+            prev = prev.next;
+            i++
+        }
+        
+        prev.next = prev.next.next;
+        return head;
     }
 }
 
-UPDATINGGG................
+
+
+
+
+
