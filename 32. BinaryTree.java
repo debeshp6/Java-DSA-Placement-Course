08c0d906c7eaf089b492336fedb1d10e1910839b
Debesh Paul <139678494+debeshp6@users.noreply.github.com>
2024-11-27 02:10:50 +0530

    Update 32. BinaryTree.java

--- 32. BinaryTree.java
+++ 32. BinaryTree.java
@@ -364,3 +364,113 @@ public class BT {
 
 
 /* Count of Nodes */
+import java.util.*;
+public class BT {
+
+	static class Node {
+		int data;
+		Node left;
+		Node right;
+
+		Node(int data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+
+	}
+
+	static class BinaryTree {
+		static int idx = -1;
+		public static Node buildTree(int nodes[]) {
+			idx++;
+			if(nodes[idx] == -1) {
+				return null;
+			}
+
+			Node newNode = new Node(nodes[idx]);
+			newNode.left = buildTree(nodes);
+			newNode.right = buildTree(nodes);
+
+			return newNode;
+		}
+	}
+
+    //count of nodes
+    public static int countOfNodes(Node root) {
+        //time complexity -> O(n)
+        if(root == null) return 0;
+        int leftNodes = countOfNodes(root.left);
+        int rightNodes = countOfNodes(root.right);
+        
+        return leftNodes + rightNodes + 1;
+    }
+    
+	public static void main (String[] args) {
+		int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+		BinaryTree tree = new BinaryTree();
+		Node root = tree.buildTree(nodes);
+		System.out.print(countOfNodes(root));
+	}
+}
+
+
+
+
+/* Sum of Nodes. */
+import java.util.*;
+
+public class BT {
+
+	static class Node {
+		int data;
+		Node left;
+		Node right;
+
+		Node(int data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+
+	}
+
+	static class BinaryTree {
+		static int idx = -1;
+		public static Node buildTree(int nodes[]) {
+			idx++;
+			if(nodes[idx] == -1) {
+				return null;
+			}
+
+			Node newNode = new Node(nodes[idx]);
+			newNode.left = buildTree(nodes);
+			newNode.right = buildTree(nodes);
+
+			return newNode;
+		}
+	}
+
+    //sum of nodes
+    //time complexity -> O(n)
+    public static int sumOfNodes(Node root) {
+        
+        if(root == null) return 0;
+        int leftSum = sumOfNodes(root.left);
+        int rightSum = sumOfNodes(root.right);
+        
+        return leftSum + rightSum + root.data;
+    }
+    
+	public static void main (String[] args) {
+		int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+		BinaryTree tree = new BinaryTree();
+		Node root = tree.buildTree(nodes);
+		System.out.print(sumOfNodes(root));
+	}
+}
+
+
+
+
+/* Height of a Tree. */
