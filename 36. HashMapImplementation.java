a6b79eb73780d407562f2b1cb8dacf9f17127ef6
Debesh Paul <139678494+debeshp6@users.noreply.github.com>
2024-11-30 05:46:30 +0530

    Update 36. HashMapImplementation.java

--- 36. HashMapImplementation.java
+++ 36. HashMapImplementation.java
@@ -1,1 +1,25 @@
-UPDATING.......
+/*
+
+implementation of HashMap:
+
+  - O(1)
+  - put()
+  - get()
+  - containsKey()
+  - remove()
+  - size()
+  - keySet()
+
+NOTE: HashMap is internally implemented as Array of LinkedList. (you must remember this)
+we can call each array index as "bucket" :)
+
+in simple language we can define hashing as -> data form change.
+example: "abc" --> 3424 (this change is done by hashing).
+
+hashing is used while making passwords. suppose, you are creating an instagram account and you set your password
+as "abcd" but in instagram's database it'll be stored as "137hfh%^h". this is how hashing works.
+
+
+
+
+*/
